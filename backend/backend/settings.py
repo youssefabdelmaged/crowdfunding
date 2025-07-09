86f1540e7d02d21db16cdee3897243168c407a94
youssefabdelmaged <youssefabdelmaged50@gmail.com>
2025-07-10 00:14:57 +0300

    update database settings to use PostgreSQL

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -99,10 +99,15 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'crowdfunding',
+        'USER': 'postgres',
+        'PASSWORD': '00800',
+        'HOST': 'localhost',   # or your remote server IP
+        'PORT': '5432',        # default PostgreSQL port
     }
 }
+
 
 
 # Password validation
